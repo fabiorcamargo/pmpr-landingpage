ee90124a017323addf5b6fefa9ee860dc5290482
Fabio Camargo <fabiorcamargo@gmail.com>
2025-04-03 11:37:06 -0300

    tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,24 @@ export const metadata = {
   description: "Faça parte do melhor preparatório para o Concurso da PMPR",
 };
 
-const GA_TRACKING_ID = "G-3FJHE8L3C1"; // Substitua pelo seu ID
+const GA_TRACKING_ID = "G-3FJHE8L3C1"; // ID do Analytics
+const GTM_ID = "GTM-MQN2N2G4"; // Substitua pelo seu ID do Tag Manager
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="pt-BR">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+
         {/* Google Analytics */}
         <Script
           strategy="afterInteractive"
@@ -29,6 +41,15 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         </Script>
       </head>
       <body>
+        {/* Fallback do GTM */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
       </body>
     </html>
